41a5f231a1a6c8dcc52e0fd2c362a5c4504727df
Dave Mackey <dave@davemackey.net>
2023-04-18 12:43:07 -0400

    Reorging types to match older code for easier merging, can reorder thereafter

--- web_client/src/types.ts
+++ web_client/src/types.ts
@@ -7,15 +7,6 @@ import type { WorkerPool } from 'itk/WorkerPool';
 // could be converted to camel case using:
 // https://morioh.com/p/8e8b33c25ea1
 
-interface ResponseData {
-  detail: string,
-  errors: string[],
-  warnings: string[],
-  default_email_recipients?: string[],
-  permissions?: string[],
-  id?: string,
-}
-
 interface User {
   id: number,
   username: string,
@@ -23,30 +14,13 @@ interface User {
   is_superuser: boolean
 }
 
-interface Email {
-  to: string[],
-  cc: string[],
-  bcc: string[],
-  subject: string,
-  body: string,
-  screenshots: any[],
-}
-
-interface Experiment {
-  id: string,
-  name: string,
-  lock_owner: {
-    id: number,
-    username: string,
-  },
-  lockOwner: {
-    id: number,
-    username: string,
-  },
-  // eslint-disable-next-line no-use-before-define
-  scans?: Scan[],
-  project: string,
-  note: string,
+interface ResponseData {
+  detail: string,
+  errors: string[],
+  warnings: string[],
+  default_email_recipients?: string[],
+  permissions?: string[],
+  id?: string,
 }
 
 interface Frame {
@@ -58,70 +32,6 @@ interface Frame {
   // TODO: Passed in from Python as { results: {}, evaluation_model: string }
   frame_evaluation?: string,
   // TODO: Also has download_url
-}
-
-interface MIQAConfig {
-  version: string,
-  artifact_states: {
-    PRESENT: boolean,
-  }
-  artifact_options?: string[],
-  auto_artifact_threshold: number,
-  NORMAL_USERS_CAN_CREATE_PROJECTS: boolean,
-  S3_SUPPORT: boolean,
-}
-
-interface Project {
-  id: string,
-  name: string,
-  experiments?: Experiment[],
-  // eslint-disable-next-line no-use-before-define
-  settings: ProjectSettings,
-  status: {
-    total_scans: number,
-    total_complete: number,
-  }
-  creator: string;
-}
-
-interface ProjectSettings {
-  import_path: string,
-  export_path: string,
-  anatomy_orientation?: string,
-  permissions?: {
-    collaborator: [],
-    tier_1_reviewer: [],
-    tier_2_reviewer: [],
-  },
-}
-
-interface ProjectTaskOverview {
-  project_id: string,
-  total_experiments: number,
-  total_scans: number,
-  my_project_role: string,
-  scan_states: {
-    string: string,
-  },
-}
-
-interface Scan {
-  id: string,
-  name: string,
-  scan_id: string,
-  scan_type: string,
-  experiment: string,
-  // eslint-disable-next-line no-use-before-define
-  decisions: ScanDecision[],
-  frames: Frame[],
-  subject_id: string,
-  subjectID: string,
-  session_id: string,
-  sessionID: string,
-  scan_link: string,
-  link: string,
-  notes: string,
-  cumulativeRange?: number,
 }
 
 interface ScanDecision {
@@ -140,10 +50,87 @@ interface ScanDecision {
   }
 }
 
+interface Scan {
+  id: string,
+  name: string,
+  scan_id: string,
+  scan_type: string,
+  experiment: string,
+  decisions: ScanDecision[],
+  frames: Frame[],
+  subject_id: string,
+  subjectID: string,
+  session_id: string,
+  sessionID: string,
+  scan_link: string,
+  link: string,
+  notes: string,
+  cumulativeRange?: number,
+}
+
+interface Experiment {
+  id: string,
+  name: string,
+  lock_owner: {
+    id: number,
+    username: string,
+  },
+  lockOwner: {
+    id: number,
+    username: string,
+  },
+  scans?: Scan[],
+  project: string,
+  note: string,
+}
+
+interface ProjectSettings {
+  import_path: string,
+  export_path: string,
+  anatomy_orientation?: string,
+  permissions?: {
+    collaborator: [],
+    tier_1_reviewer: [],
+    tier_2_reviewer: [],
+  },
+  artifacts?: Array<string>[],
+}
+
 enum ScanState {
   unreviewed = '#1460A3',
   needs_tier_2_review = '#6DB1ED',
   complete = '#00C853',
+}
+
+interface ProjectTaskOverview {
+  project_id: string,
+  total_experiments: number,
+  total_scans: number,
+  my_project_role: string,
+  scan_states: {
+    string: string,
+  },
+}
+
+interface Project {
+  id: string,
+  name: string,
+  experiments?: Experiment[],
+  settings: ProjectSettings,
+  status: {
+    total_scans: number,
+    total_complete: number,
+  }
+  creator: string;
+}
+
+interface Email {
+  to: string[],
+  cc: string[],
+  bcc: string[],
+  subject: string,
+  body: string,
+  screenshots: any[],
 }
 
 interface WindowLock {
@@ -151,6 +138,17 @@ interface WindowLock {
   duration?: string;
   target?: string;
   associatedImage?: string;
+}
+
+interface MIQAConfig {
+  version: string,
+  artifact_states: {
+    PRESENT: boolean,
+  }
+  artifact_options?: string[],
+  auto_artifact_threshold: number,
+  NORMAL_USERS_CAN_CREATE_PROJECTS: boolean,
+  S3_SUPPORT: boolean,
 }
 
 interface MIQAStore {
